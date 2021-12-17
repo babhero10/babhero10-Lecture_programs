77b9068dcd9b3c805de9f8bbf151826571a5af46
babhero10 <abdallah.elsaid25@gmail.com>
2021-12-17 15:35:27 +0200

    Finish version1

--- catToDog.c
+++ catToDog.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 int catToDog(char text[]);
 
@@ -21,6 +22,27 @@ void main()
     printf("Number of words edited: %i \nThe text: %s", count, text);
 }
 
+// Search for a word in a text returns the address of first letter of the word
+char *search_for_word(char text[], char word[])
+{
+    const unsigned char length_of_word = strlen(word);
+    char *first_char_adress = strstr(text, word);
+    
+    while (first_char_adress != NULL)
+    {
+        if (!isalpha(*(first_char_adress - 1)) &&
+           (!isalpha(*(first_char_adress + length_of_word)) || *(first_char_adress + length_of_word) == '\0'))
+        {
+            return first_char_adress;
+        }
+        
+        first_char_adress = strstr(first_char_adress + 1, word);
+    }
+    
+    return NULL;
+}
+
+// Chagne all the word cat into dog
 int catToDog(char text[])
 {
     char *nextCat = text;
@@ -28,8 +50,8 @@ int catToDog(char text[])
 
     for (int i = 0; text[i] != '\0'; i++)
     {
-        nextCat = strstr(nextCat, "cat");
-        
+        nextCat = search_for_word(nextCat, "cat");
+    
         if (nextCat == NULL)
             break;
         
